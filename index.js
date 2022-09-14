b9809bb1e248e6eb83350c527ddd8fdfea87be82
nicolas andrade <nicolasandrade328@gmail.com>
2022-09-13 22:17:25 -0300

    agregamos rutas de prueba

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 import express from 'express';
 
-console.log('hola mundo')
+//instancia de express
+const app = express();
+
+//queremos tomar el puerto de la pc
+app.set('port', process.env.PORT || 4000);
+
+//quiero que mi backend escuche el puerto
+app.listen(app.get('port'), () => {
+    console.log('Estamos en el puerto ' + app.get('port'));
+});
+ //middlewares
+
+//rutas
+
+app.get('/',(req,res)=>{
+ res.send('primera peticion get')
+})
