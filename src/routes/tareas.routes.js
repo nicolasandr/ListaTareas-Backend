ef6ff93f1b83837efb57461cf09d4bff0abc36fe
nicolas andrade <nicolasandrade328@gmail.com>
2022-09-14 00:47:28 -0300

    reacomodamos las rutas de los productos

--- src/routes/tareas.routes.js
+++ src/routes/tareas.routes.js
@@ -1,12 +1,13 @@
 
 import { Router } from 'express';
-
+import { crearProducto, listarProducto, obtenerProducto } from '../controllers/productos.controller';
 const router = Router();
 //rutas de los productos
 router
 .route('/')
-.get((req, res) => {
-    res.send('primera peticion get');
-});
+.get(listarProducto)
+.post(crearProducto);
 export default router;
 
+router.route('/productos/:id')
+.get(obtenerProducto).put().delete();
