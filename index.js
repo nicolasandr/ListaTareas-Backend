26e20cb02cf18923e4d4f209424249d28c4765e7
nicolas andrade <nicolasandrade328@gmail.com>
2022-09-13 23:04:52 -0300

    agregamos middlewares para diferentes configuraciones

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from 'express';
-
+import morgan from 'morgan';
+import cors from 'cors';
 //instancia de express
 const app = express();
 
@@ -10,7 +11,13 @@ app.set('port', process.env.PORT || 4000);
 app.listen(app.get('port'), () => {
     console.log('Estamos en el puerto ' + app.get('port'));
 });
- //middlewares
+ //middlewares (funciones pequeñas para una actividad puntual)
+app.use(morgan('dev'));
+app.use(cors('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+//cargamos archivo estatico
+app.use(express.static('./public'));
 
 //rutas
 
